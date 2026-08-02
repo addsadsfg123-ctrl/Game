a6ecf406f175dc3f05a0a5e2de18a2613f115f62
addsadsfg123-ctrl <addsadsfg123@gmail.com>
2026-08-03 00:20:06 +0300

    Refactor renderPlayer and showScene functions

--- app.js
+++ app.js
@@ -1,55 +1,61 @@
-function renderPlayer(){
+function renderPlayer() {
 
-document.getElementById("playerBar").innerHTML=
+document.getElementById("playerBar").innerHTML = `
+👤 ${player.name}<br><br>
 
-`
 ❤️ ${player.hp}
-
 &nbsp;&nbsp;
-
 🪙 ${player.gold}
-
 &nbsp;&nbsp;
-
 🗡️ ${player.strength}
-
 &nbsp;&nbsp;
-
 🏃 ${player.agility}
-
 &nbsp;&nbsp;
-
 💬 ${player.charisma}
 `;
 
 }
 
-function showScene(id){
+function showScene(id) {
 
-const scene=STORY[id];
+const scene = STORY[id];
 
-document.getElementById("story").innerHTML=scene.text;
+document.getElementById("story").innerHTML = scene.text;
 
-let html="";
+let html = "";
 
-scene.choices.forEach(choice=>{
+scene.choices.forEach(choice => {
 
-html+=`
-
+html += `
 <button onclick="showScene('${choice.next}')">
-
 ${choice.text}
-
 </button>
-
 `;
 
 });
 
-document.getElementById("choices").innerHTML=html;
+document.getElementById("choices").innerHTML = html;
 
 renderPlayer();
 
 }
 
+function startGame() {
+
+const name = prompt("Введите имя героя:");
+
+if (name && name.trim() !== "") {
+
+player.name = name;
+
+} else {
+
+player.name = "Безымянный";
+
+}
+
 showScene("start");
+
+}
+
+startGame();
